25a2f96f6827cbbb7937e6d92a5e570040ae43f9
Student <s525138@ol10016m.nwmissouri.edu>
2016-03-27 20:16:14 -0500

    Initial Commit

---
+++ Yadav_MultilingualAppTests/Yadav_MultilingualAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Yadav_MultilingualAppTests.swift
+//  Yadav_MultilingualAppTests
+//
+//  Created by Yadav,Shalu on 3/27/16.
+//  Copyright © 2016 Yadav,Shalu. All rights reserved.
+//
+
+import XCTest
+@testable import Yadav_MultilingualApp
+
+class Yadav_MultilingualAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
